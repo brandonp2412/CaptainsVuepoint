0354952ee5210596652779a73e279135557a69e0
Brandon Presley <brandon@presley.nz>
2024-11-24 10:21:25 +1300

    Update homepage

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,17 +4,13 @@ defineProps({
     type: String,
     required: true,
   },
-})
+});
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+    <h3>This is the Vue learning module for CodeSail</h3>
   </div>
 </template>
 
