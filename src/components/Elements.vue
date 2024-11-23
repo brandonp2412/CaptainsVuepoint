c59d605b1a2efdb3a8a0a2d27a8be67e21b9e2f3
Brandon Presley <brandon@presley.nz>
2024-11-24 10:53:12 +1300

    Add funny cat to elements lesson

--- src/components/Elements.vue
+++ src/components/Elements.vue
@@ -1,6 +1,16 @@
 <template>
   <h1>Elements lesson</h1>
+  <p>HTML elements work the same way in vue</p>
 
+  <p>Paragraphs start with <code>p</code></p>
+
+  <!-- Replace this with a different image -->
+  <img src="../assets/funny-cat.jpg" height="200px" />
+
+  <!-- This adds space -->
+  <br />
+
+  <!-- These navigate to locations -->
   <RouterLink to="/">Go back</RouterLink>
   <RouterLink to="/templates">Next lesson</RouterLink>
 </template>
